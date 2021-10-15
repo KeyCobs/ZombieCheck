a18575afc166d6ba9f74cfd80bc374decd1f0a44
KeyCobs <45495774+KeyCobs@users.noreply.github.com>
2021-10-15 19:41:34 +0200

    Cloning rest

--- Assets/Script/PathManagement.cs
+++ Assets/Script/PathManagement.cs
@@ -11,29 +11,27 @@ namespace Assets.Script
     class PathManagement
     {
         
-        public void CreatePath(Texture tex, Vector3 vec)
+        public void GenerateMovements(Texture tex, Vector3 vec)
         {
             //Creating + placing texture
             GameObject path = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            vec.y += 1.0f;
+            path.transform.position += vec; 
             MeshRenderer meshRenderer = path.GetComponent<MeshRenderer>();
             meshRenderer.material.mainTexture = tex;
             TransforPath(vec);
         }
 
-        private void TransforPath(Vector3 vec)
+        private Vector3 TransforPath(Vector3 vec)
         {
             Vector3 currentPos = vec;
-            currentPos.y += 1;
-            m_PathPawn.transform.position = currentPos;
+            currentPos.y += 8.0f;
+            return currentPos;
         }
 
-        public void TogglePathCreation()
-        {
-            m_PathDestory = !m_PathDestory;
-        }
 
-        public GameObject m_PathPawn { get; private set; }
-        public bool m_PathDestory { get; private set; } = true;
+       
+        public bool m_PawnFirstMove { get; private set; } = true;
 
     }
 
--- Assets/Script/PawnSelection.cs
+++ Assets/Script/PawnSelection.cs
@@ -3,62 +3,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PawnSelection : MonoBehaviour
 {
     //public
     public Texture g_Texture;
+    // public PawnSelection.PawnType g_type = new PawnSelection.PawnType(); 
     //private
+    private bool g_isFirstMove;
+    public Pawn g_Pawn;
 
-    private  PawnActionControls pawnActionControls;
 
-    private void Awake()
-    {
-        pawnActionControls = new PawnActionControls();
-    }
+    ////  private  PawnActionControls pawnActionControls;
 
-    private void OnEnable()
-    {
-        pawnActionControls.Enable();
-        
-    }
+    //private void Awake()
+    //{
+    //    //  pawnActionControls = new PawnActionControls();
+    //}
 
-    private void OnDisable()
-    {
-        pawnActionControls.Disable();
-    }
+    //private void OnEnable()
+    //{
+    //    //  pawnActionControls.Enable();
+
+    //}
+
+    //private void OnDisable()
+    //{
+    //    // pawnActionControls.Disable();
+    //}
     private void Start()
     {
-
+        Vector2 pos = new Vector2(0.5f,1.5f);
+        string texPath = "Assets/Textures/Path_Init.png";
+        g_Pawn = new Pawn(texPath, PawnType.pawn,pos,true);
     }
-
-    private void Update()
+    private void OnMouseDown()
     {
+        g_Pawn.TestRun();
         PathManagement pathManagement = new PathManagement();
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            g_Pawn.TestRun();
+
             //Read the movement value
-            float movementInput = pawnActionControls.Pawn.Move.ReadValue<float>();
+            // float movementInput = pawnActionControls.Pawn.Move.ReadValue<float>();
             // Move the player
             Vector3 currentPos = transform.position;
             //currentPos.y += 2;
             //transform.position = currentPos;
             //Creating our path for our pawn
 
-            pathManagement.CreatePath(g_Texture, currentPos);
-            pathManagement.TogglePathCreation();
-            //delete if clicked again
-            if (pathManagement.m_PathDestory)
-            {
-                print(pathManagement.m_PathPawn);
-                Destroy(pathManagement.m_PathPawn);
-                print(pathManagement.m_PathPawn);
-            }
+            pathManagement.GenerateMovements(g_Texture, currentPos);
         }
-
+    }
+    private void Update()
+    {
 
 
 
 
     }
+
+    //private void GeneratePath()
+    //{
+    //    Vector2[] virtuelPos;
+    //    switch (0)
+    //    {
+    //        case "pawn":
+    //            break;
+
+    //        default:
+    //    }
+
+    //}
+
 }
+
+
