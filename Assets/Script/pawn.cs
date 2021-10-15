6cf330b5df7aaf41af3043920f2f88cd27deadab
KeyCobs <45495774+KeyCobs@users.noreply.github.com>
2021-10-15 19:41:21 +0200

    Creating pawn and added future code
    Creating Pawn

---
+++ Assets/Script/pawn.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Script;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Assets.Script
+{
+    public class Pawn : MonoBehaviour
+    {
+        public Pawn(string texPath, PawnType type, Vector2 pos, bool isHuman)
+        {
+            m_Type = type;
+            m_Pos = pos;
+            m_IsFirstMove = true;
+            m_IsHuman = isHuman;
+        }
+
+        public void TestRun()
+        {
+            print("Testing this script");
+        }
+
+        //public
+        [SerializeField]
+        public Texture m_Texture;
+        public Texture m_PathText;
+        //[SerializeField]
+
+        public PawnType m_Type;
+        public Vector2 m_Pos;
+        //private
+        private bool m_IsFirstMove;
+        private bool m_IsHuman; // White = human or black = Zombie;
+    }
+}
---
+++ Assets/Script/pawnManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script
+{
+    public enum PawnType
+    {
+        pawn,
+        bishop,
+        queen,
+        king,
+        rook,
+        knight
+    }
+
+    class pawnManagement
+    {
+
+        private Pawn[] m_Pawns = new Pawn[16];
+
+    }
+}
